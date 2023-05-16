efadf32640cf38f766674b4a518c36675480a002
charles5991 <charleschan5991@gmail.com>
2023-05-17 04:21:38 +0800

    new commit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,140 +1,227 @@
+"use client";
 import Link from "next/link";
-import React from "react";
-import { allProjects } from "contentlayer/generated";
+import React, { useState, useEffect } from "react";
+import { Chart } from "chart.js";
+import * as Form from "@radix-ui/react-form";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import { Article } from "./article";
-import { Redis } from "@upstash/redis";
-import { Eye } from "lucide-react";
-
-const redis = Redis.fromEnv();
+import {
+  FormControl,
+  InputLabel,
+  Slider,
+  Box,
+  Typography,
+  Input,
+  Grid,
+  CardContent,
+  InputAdornment,
+} from "@mui/material";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 
 export const revalidate = 60;
-export default async function ProjectsPage() {
-	const views = (
-		await redis.mget<number[]>(
-			...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
-		)
-	).reduce((acc, v, i) => {
-		acc[allProjects[i].slug] = v ?? 0;
-		return acc;
-	}, {} as Record<string, number>);
+export default function ProjectsPage() {
+  const [budget, setBudget] = useState<number>(200);
+  const [cpc, setCpc] = useState<number>(1);
+  const [conversionRate, setConversionRate] = useState<number>(10);
+  const [purchaseRate, setPurchaseRate] = useState<number>(10);
+  const [avgSale, setAvgSale] = useState<number>(200);
+  const [data, setData] = useState<Array<{ name: string; roas: number }>>([]);
 
-	const featured = allProjects.find(
-		(project) => project.slug === "planetfall",
-	)!;
-	const top2 = allProjects.find((project) => project.slug === "envshare")!;
-	const top3 = allProjects.find((project) => project.slug === "qstash")!;
-	const sorted = allProjects
-		.filter((p) => p.published)
-		.filter(
-			(project) =>
-				project.slug !== featured.slug &&
-				project.slug !== top2.slug &&
-				project.slug !== top3.slug,
-		)
-		.sort(
-			(a, b) =>
-				new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-				new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
-		);
+  useEffect(() => {
+    const clicks = Math.floor(budget / cpc);
+    const conversions = Math.floor((clicks * conversionRate) / 100);
+    const costPerConversion = budget / conversions;
+    const purchases = Math.floor((conversions * purchaseRate) / 100);
+    const revenue = purchases * avgSale;
+    const roas = (((revenue - budget) / budget) * 100).toFixed(2);
 
-	return (
-		<div className="relative pb-16">
-			<Navigation />
-			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-				<div className="max-w-2xl mx-auto lg:mx-0">
-					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						Projects
-					</h2>
-					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
-					</p>
-				</div>
-				<div className="w-full h-px bg-zinc-800" />
+    const newData = [{ name: "ROAS", roas: Number(roas) }];
+    setData(newData);
+  }, [budget, cpc, conversionRate, purchaseRate, avgSale]);
 
-				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-					<Card>
-						<Link href={`/projects/${featured.slug}`}>
-							<article className="relative h-full w-full p-4 md:p-8">
-								<div className="flex justify-between gap-2 items-center">
-									<div className="text-xs text-zinc-100">
-										{featured.date ? (
-											<time dateTime={new Date(featured.date).toISOString()}>
-												{Intl.DateTimeFormat(undefined, {
-													dateStyle: "medium",
-												}).format(new Date(featured.date))}
-											</time>
-										) : (
-											<span>SOON</span>
-										)}
-									</div>
-									<span className="text-zinc-500 text-xs  flex items-center gap-1">
-										<Eye className="w-4 h-4" />{" "}
-										{Intl.NumberFormat("en-US", { notation: "compact" }).format(
-											views[featured.slug] ?? 0,
-										)}
-									</span>
-								</div>
+  return (
+    <div className="relative pb-16">
+      <Navigation />
+      <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
+            The Advertising ROI Calculator{" "}
+          </h2>
+          <p className="mt-4 text-black">
+            Paying for clicks to your website is a risk. Who knows if it'll pay
+            off? It can feel like buying a lottery ticket. But it doesn’t have
+            to. Plan for ad spend success by estimating a few key metrics. Don't
+            gamble with your business. Know what kind of ROI you can expect.{" "}
+          </p>
+        </div>
+        <div className="w-full h-px bg-black" />
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+          <Card>
+            <Typography
+              id="budget-slider"
+              className="font-semibold"
+              gutterBottom
+            >
+              Budget
+            </Typography>
+            <Slider
+              value={budget}
+              onChange={(e, newValue) => setBudget(newValue as number)}
+              aria-labelledby="budget-slider"
+              step={10}
+              min={0}
+              max={10000}
+              valueLabelDisplay="auto"
+            />
+            <Input
+              value={budget}
+              margin="dense"
+              onChange={(e) => setBudget(+e.target.value)}
+              style={{ fontWeight: "bolder" }}
+              inputProps={{
+                step: 10,
+                min: 0,
+                max: 1000,
+                type: "number",
+              }}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+            />
 
-								<h2
-									id="featured-post"
-									className="mt-4 text-3xl font-bold  text-zinc-100 group-hover:text-white sm:text-4xl font-display"
-								>
-									{featured.title}
-								</h2>
-								<p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-									{featured.description}
-								</p>
-								<div className="absolute bottom-4 md:bottom-8">
-								<p className="text-zinc-200 hover:text-zinc-50 hidden lg:block">
-										Read more <span aria-hidden="true">&rarr;</span>
-									</p>
-								</div>
-							</article>
-						</Link>
-					</Card>
+            <Typography id="cpc-slider" className="font-semibold" gutterBottom>
+              CPC
+            </Typography>
+            <Slider
+              value={cpc}
+              onChange={(e, newValue) => setCpc(newValue as number)}
+              aria-labelledby="cpc-slider"
+              step={0.1}
+              min={0}
+              max={1000}
+              valueLabelDisplay="auto"
+            />
+            <Input
+              value={cpc}
+              margin="dense"
+              onChange={(e) => setCpc(+e.target.value)}
+              inputProps={{
+                step: 0.1,
+                min: 0,
+                max: 10,
+                type: "number",
+              }}
+            />
 
-					<div className="flex flex-col w-full gap-8  mx-auto border-t border-gray-900/10  lg:mx-0  lg:border-t-0 ">
-						{[top2, top3].map((project) => (
-							<Card key={project.slug}>
-								<Article project={project} views={views[project.slug] ?? 0} />
-							</Card>
-						))}
-					</div>
-				</div>
-				<div className="hidden w-full h-px md:block bg-zinc-800" />
+            <Typography
+              id="avg-sale-slider"
+              className="font-semibold"
+              gutterBottom
+            >
+              Average Sale Per Customer
+            </Typography>
+            <Slider
+              value={avgSale}
+              onChange={(e, newValue) => setAvgSale(newValue as number)}
+              aria-labelledby="avg-sale-slider"
+              step={10}
+              min={0}
+              max={1000}
+              valueLabelDisplay="auto"
+            />
+            <Input
+              value={avgSale}
+              margin="dense"
+              onChange={(e) => setAvgSale(+e.target.value)}
+              inputProps={{
+                step: 10,
+                min: 0,
+                max: 1000,
+                type: "number",
+              }}
+            />
 
-				<div className="grid  grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 0)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 1)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 2)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
-								</Card>
-							))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <Typography
+              id="conversion-rate-slider"
+              className="font-semibold"
+              gutterBottom
+            >
+              Estimated Conversion Rate
+            </Typography>
+            <Slider
+              value={conversionRate}
+              onChange={(e, newValue) => setConversionRate(newValue as number)}
+              aria-labelledby="conversion-rate-slider"
+              step={1}
+              min={0}
+              max={100}
+              valueLabelDisplay="auto"
+            />
+            <Input
+              value={conversionRate}
+              margin="dense"
+              onChange={(e) => setConversionRate(+e.target.value)}
+              inputProps={{
+                step: 1,
+                min: 0,
+                max: 100,
+                type: "number",
+              }}
+            />
+
+            <Typography
+              id="purchase-rate-slider"
+              className="font-semibold"
+              gutterBottom
+            >
+              Conversion to Purchase %
+            </Typography>
+            <Slider
+              value={purchaseRate}
+              onChange={(e, newValue) => setPurchaseRate(newValue as number)}
+              aria-labelledby="purchase-rate-slider"
+              step={1}
+              min={0}
+              max={100}
+              valueLabelDisplay="auto"
+            />
+            <Input
+              value={purchaseRate}
+              margin="dense"
+              onChange={(e) => setPurchaseRate(+e.target.value)}
+              inputProps={{
+                step: 1,
+                min: 0,
+                max: 100,
+                type: "number",
+              }}
+            />
+          </Card>
+
+          <div className="flex flex-col w-full gap-8  mx-auto border-t border-gray-900/10  lg:mx-0  lg:border-t-0 ">
+            <Card>
+              123
+              {/* <Typography variant="h5">ROAS: {data[0].roas}%</Typography> */}
+            </Card>
+            <Card>
+              {data.length > 0 ? (
+                <>
+                  <BarChart width={300} height={300} data={data}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Bar dataKey="roas" fill="#ff7300" />
+                  </BarChart>
+                </>
+              ) : (
+                <Typography variant="h5">ROAS: N/A</Typography>
+              )}
+            </Card>
+          </div>
+        </div>
+        <div className="hidden w-full h-px md:block bg-white" />
+      </div>
+    </div>
+  );
 }
