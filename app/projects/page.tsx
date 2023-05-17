f03194a5e2f2999877c8d820bc5f080acdd3b914
charles5991 <charleschan5991@gmail.com>
2023-05-17 18:18:42 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,43 +1,28 @@
 "use client";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
-import { Chart } from "chart.js";
-import * as Form from "@radix-ui/react-form";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import {
-  FormControl,
-  InputLabel,
-  Slider,
-  Box,
-  Typography,
-  Input,
-  Grid,
-  CardContent,
-  InputAdornment,
-} from "@mui/material";
+
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 
 export const revalidate = 60;
 export default function ProjectsPage() {
-  const [budget, setBudget] = useState<number>(200);
-  const [cpc, setCpc] = useState<number>(1);
-  const [conversionRate, setConversionRate] = useState<number>(10);
-  const [purchaseRate, setPurchaseRate] = useState<number>(10);
-  const [avgSale, setAvgSale] = useState<number>(200);
-  const [data, setData] = useState<Array<{ name: string; roas: number }>>([]);
+  const [budget, setBudget] = useState(0);
+  const [cpc, setCPC] = useState(0);
+  const [conversionRate, setConversionRate] = useState(0);
+  const [conversionToPurchase, setConversionToPurchase] = useState(0);
+  const [averageSale, setAverageSale] = useState(0);
+  const [roas, setRoas] = useState(0);
 
   useEffect(() => {
-    const clicks = Math.floor(budget / cpc);
-    const conversions = Math.floor((clicks * conversionRate) / 100);
-    const costPerConversion = budget / conversions;
-    const purchases = Math.floor((conversions * purchaseRate) / 100);
-    const revenue = purchases * avgSale;
-    const roas = (((revenue - budget) / budget) * 100).toFixed(2);
+    const clicks = budget / cpc;
+    const conversions = clicks * (conversionRate / 100);
+    const purchases = conversions * (conversionToPurchase / 100);
+    const revenue = purchases * averageSale;
 
-    const newData = [{ name: "ROAS", roas: Number(roas) }];
-    setData(newData);
-  }, [budget, cpc, conversionRate, purchaseRate, avgSale]);
+    setRoas(revenue / budget);
+  }, [budget, cpc, conversionRate, conversionToPurchase, averageSale]);
 
   return (
     <div className="relative pb-16">
@@ -57,167 +42,95 @@ export default function ProjectsPage() {
         <div className="w-full h-px bg-black" />
         <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
           <Card>
-            <Typography
-              id="budget-slider"
-              className="font-semibold"
-              gutterBottom
-            >
-              Budget
-            </Typography>
-            <Slider
-              value={budget}
-              onChange={(e, newValue) => setBudget(newValue as number)}
-              aria-labelledby="budget-slider"
-              step={10}
-              min={0}
-              max={10000}
-              valueLabelDisplay="auto"
-            />
-            <Input
-              value={budget}
-              margin="dense"
-              onChange={(e) => setBudget(+e.target.value)}
-              style={{ fontWeight: "bolder" }}
-              inputProps={{
-                step: 10,
-                min: 0,
-                max: 1000,
-                type: "number",
-              }}
-              startAdornment={
-                <InputAdornment position="start">$</InputAdornment>
-              }
-            />
+            <div className="p-4 space-y-4">
+              <div>
+                <label className="block text-gray-700">
+                  Budget:
+                  <input
+                    type="range"
+                    min="0"
+                    max="1000"
+                    value={budget}
+                    onChange={(e) => setBudget(Number(e.target.value))}
+                    className="block w-full mt-1"
+                  />
+                </label>
+                <p>Your Budget: {budget} USD</p>
+              </div>
 
-            <Typography id="cpc-slider" className="font-semibold" gutterBottom>
-              CPC
-            </Typography>
-            <Slider
-              value={cpc}
-              onChange={(e, newValue) => setCpc(newValue as number)}
-              aria-labelledby="cpc-slider"
-              step={0.1}
-              min={0}
-              max={1000}
-              valueLabelDisplay="auto"
-            />
-            <Input
-              value={cpc}
-              margin="dense"
-              onChange={(e) => setCpc(+e.target.value)}
-              inputProps={{
-                step: 0.1,
-                min: 0,
-                max: 10,
-                type: "number",
-              }}
-            />
+              <div>
+                <label className="block text-gray-700">
+                  Cost per Click (CPC):
+                  <input
+                    type="range"
+                    min="0"
+                    max="10"
+                    value={cpc}
+                    onChange={(e) => setCPC(Number(e.target.value))}
+                    className="block w-full mt-1"
+                  />
+                </label>
+                <p>Your CPC: {cpc} USD</p>
+              </div>
 
-            <Typography
-              id="avg-sale-slider"
-              className="font-semibold"
-              gutterBottom
-            >
-              Average Sale Per Customer
-            </Typography>
-            <Slider
-              value={avgSale}
-              onChange={(e, newValue) => setAvgSale(newValue as number)}
-              aria-labelledby="avg-sale-slider"
-              step={10}
-              min={0}
-              max={1000}
-              valueLabelDisplay="auto"
-            />
-            <Input
-              value={avgSale}
-              margin="dense"
-              onChange={(e) => setAvgSale(+e.target.value)}
-              inputProps={{
-                step: 10,
-                min: 0,
-                max: 1000,
-                type: "number",
-              }}
-            />
+              <div>
+                <label className="block text-gray-700">
+                  Estimated Conversion Rate (%):
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={conversionRate}
+                    onChange={(e) => setConversionRate(Number(e.target.value))}
+                    className="block w-full mt-1"
+                  />
+                </label>
+                <p>Your Estimated Conversion Rate: {conversionRate} %</p>
+              </div>
 
-            <Typography
-              id="conversion-rate-slider"
-              className="font-semibold"
-              gutterBottom
-            >
-              Estimated Conversion Rate
-            </Typography>
-            <Slider
-              value={conversionRate}
-              onChange={(e, newValue) => setConversionRate(newValue as number)}
-              aria-labelledby="conversion-rate-slider"
-              step={1}
-              min={0}
-              max={100}
-              valueLabelDisplay="auto"
-            />
-            <Input
-              value={conversionRate}
-              margin="dense"
-              onChange={(e) => setConversionRate(+e.target.value)}
-              inputProps={{
-                step: 1,
-                min: 0,
-                max: 100,
-                type: "number",
-              }}
-            />
+              <div>
+                <label className="block text-gray-700">
+                  Conversion to Purchase Rate (%):
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={conversionToPurchase}
+                    onChange={(e) =>
+                      setConversionToPurchase(Number(e.target.value))
+                    }
+                    className="block w-full mt-1"
+                  />
+                </label>
+                <p>
+                  Your Conversion to Purchase Rate: {conversionToPurchase} %
+                </p>
+              </div>
 
-            <Typography
-              id="purchase-rate-slider"
-              className="font-semibold"
-              gutterBottom
-            >
-              Conversion to Purchase %
-            </Typography>
-            <Slider
-              value={purchaseRate}
-              onChange={(e, newValue) => setPurchaseRate(newValue as number)}
-              aria-labelledby="purchase-rate-slider"
-              step={1}
-              min={0}
-              max={100}
-              valueLabelDisplay="auto"
-            />
-            <Input
-              value={purchaseRate}
-              margin="dense"
-              onChange={(e) => setPurchaseRate(+e.target.value)}
-              inputProps={{
-                step: 1,
-                min: 0,
-                max: 100,
-                type: "number",
-              }}
-            />
+              <div>
+                <label className="block text-gray-700">
+                  Average Sale per Customer:
+                  <input
+                    type="range"
+                    min="0"
+                    max="1000"
+                    value={averageSale}
+                    onChange={(e) => setAverageSale(Number(e.target.value))}
+                    className="block w-full mt-1"
+                  />
+                </label>
+                <p>Your Average Sale per Customer: {averageSale} USD</p>
+              </div>
+
+              <p className="text-xl font-bold">ROAS: {roas}</p>
+            </div>
           </Card>
 
           <div className="flex flex-col w-full gap-8  mx-auto border-t border-gray-900/10  lg:mx-0  lg:border-t-0 ">
             <Card>
-              123
-              {/* <Typography variant="h5">ROAS: {data[0].roas}%</Typography> */}
+              <p className="text-xl font-bold">ROAS: {roas}</p>
             </Card>
-            <Card>
-              {data.length > 0 ? (
-                <>
-                  <BarChart width={300} height={300} data={data}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <CartesianGrid stroke="#f5f5f5" />
-                    <Bar dataKey="roas" fill="#ff7300" />
-                  </BarChart>
-                </>
-              ) : (
-                <Typography variant="h5">ROAS: N/A</Typography>
-              )}
-            </Card>
+            <Card>123</Card>
           </div>
         </div>
         <div className="hidden w-full h-px md:block bg-white" />
