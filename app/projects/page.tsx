fa54ea9f46fa4e7247d344df5587dd17473507a9
charles5991 <charleschan5991@gmail.com>
2023-06-15 04:24:57 +0800

    Update Calculation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,15 @@
 "use client";
 import React, { useState, useEffect } from "react";
-// import { Card } from "../components/card";
 import { Card } from "../components/card";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
-// import { Slider } from "@radix-ui/react-slider";
 import * as Slider from "@radix-ui/react-slider";
 
 export const revalidate = 60;
 export default function ProjectsPage() {
-  const [budget, setBudget] = useState<[number]>([0]);
-  const [cpc, setCPC] = useState<[number]>([0]);
-  const [conversionRate, setConversionRate] = useState<[number]>([0]);
-  const [conversionToPurchase, setConversionToPurchase] = useState<[number]>([
-    0,
-  ]);
-  const [averageSale, setAverageSale] = useState<[number]>([0]);
+  const [budget, setBudget] = useState<number>(0);
+  const [cpc, setCPC] = useState<number>(0);
+  const [conversionRate, setConversionRate] = useState<number>(0);
+  const [conversionToPurchase, setConversionToPurchase] = useState<number>(0);
+  const [averageSale, setAverageSale] = useState<number>(0);
   const [roas, setRoas] = useState<number>(0);
   const [clicks, setClicks] = useState<number>(0);
   const [conversions, setConversions] = useState<number>(0);
@@ -22,18 +17,12 @@ export default function ProjectsPage() {
   const [revenue, setRevenue] = useState<number>(0);
 
   useEffect(() => {
-    const budgetValue = budget[0];
-    const cpcValue = cpc[0];
-    const conversionRateValue = conversionRate[0];
-    const conversionToPurchaseValue = conversionToPurchase[0];
-    const averageSaleValue = averageSale[0];
-
     if (
-      budgetValue === 0 ||
-      cpcValue === 0 ||
-      conversionRateValue === 0 ||
-      conversionToPurchaseValue === 0 ||
-      averageSaleValue === 0
+      budget === 0 ||
+      cpc === 0 ||
+      conversionRate === 0 ||
+      conversionToPurchase === 0 ||
+      averageSale === 0
     ) {
       setRoas(0);
       setClicks(0);
@@ -43,47 +32,34 @@ export default function ProjectsPage() {
       return;
     }
 
-    const calculatedClicks = budgetValue / cpcValue;
-    const calculatedConversions =
-      calculatedClicks * (conversionRateValue / 100);
+    const calculatedClicks = budget / cpc;
+    const calculatedConversions = calculatedClicks * (conversionRate / 100);
+    const calculatedCostPerConversion = budget / calculatedConversions;
     const calculatedPurchases =
-      calculatedConversions * (conversionToPurchaseValue / 100);
-    const calculatedRevenue = calculatedPurchases * averageSaleValue;
+      calculatedConversions * (conversionToPurchase / 100);
+    const calculatedRevenue = calculatedPurchases * averageSale;
 
     setClicks(calculatedClicks);
     setConversions(calculatedConversions);
     setPurchases(calculatedPurchases);
     setRevenue(calculatedRevenue);
-    setRoas((calculatedRevenue / budgetValue) * 100);
-    // setRoas((revenue / budgetValue) * 1);
+    setRoas((calculatedRevenue / budget) * 100);
   }, [budget, cpc, conversionRate, conversionToPurchase, averageSale]);
 
   return (
     <div className="relative pb-16">
-      {/* <Navigation /> */}
       <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-        {/* <div className="max-w-2xl mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
-            The Advertising ROI Calculator
-          </h2>
-          <p className="mt-4 text-black">
-            Paying for clicks to your website is a risk. Who knows if it'll pay
-            off? It can feel like buying a lottery ticket. But it doesn’t have
-            to. Plan for ad spend success by estimating a few key metrics. Don't
-            gamble with your business. Know what kind of ROI you can expect.
-          </p>
-        </div>
-        <div className="w-full h-px bg-black" /> */}
         <div className="md:grid md:grid-cols-1 gap-8 mx-auto text-center lg:grid-cols-1">
           <div className="p-4 space-y-4">
             <div>
               <Card>
-                <div className=" space-y-4 flex-grow-0 flex-shrink-0">
+                <div className="space-y-4 flex-grow-0 flex-shrink-0">
                   <div className="flex items-center bg-blue-800">
                     <img
                       src="./budget.png"
                       width={70}
                       className="my-6 mx-8"
+                      alt="Budget"
                     ></img>
                     <p className="text-white w-50 text-lg font-semibold">
                       YOUR BUDGET
@@ -101,8 +77,8 @@ export default function ProjectsPage() {
                     <p className="font-bold text-2xl text-black">$ {budget}</p>
                     <Slider.Root
                       className="relative flex items-center w-full h-6 select-none"
-                      value={[budget[0]]}
-                      onValueChange={(newValues) => setBudget([newValues[0]])}
+                      value={[budget]}
+                      onValueChange={(newValues) => setBudget(newValues[0])}
                       min={0}
                       max={50000}
                       step={10}
@@ -126,9 +102,14 @@ export default function ProjectsPage() {
 
             <div>
               <Card>
-                <div className=" space-y-4 ">
+                <div className="space-y-4 ">
                   <div className="flex items-center bg-blue-800">
-                    <img src="./cpc.png" width={50} className="my-6 mx-8"></img>
+                    <img
+                      src="./cpc.png"
+                      width={50}
+                      className="my-6 mx-8"
+                      alt="CPC"
+                    ></img>
                     <p className="text-white w-50 text-lg font-semibold">
                       YOUR CPC
                     </p>
@@ -147,8 +128,8 @@ export default function ProjectsPage() {
                       min={0.1}
                       max={20}
                       step={0.1}
-                      value={[cpc[0]]}
-                      onValueChange={(newValues) => setCPC([newValues[0]])}
+                      value={[cpc]}
+                      onValueChange={(newValues) => setCPC(newValues[0])}
                     >
                       <Slider.Track className="relative flex-grow h-2 bg-gray-200 rounded-full outline-none">
                         <Slider.Range className="absolute h-full bg-red-600 rounded-full outline-none" />
@@ -169,12 +150,13 @@ export default function ProjectsPage() {
 
             <div>
               <Card>
-                <div className=" space-y-4">
+                <div className="space-y-4">
                   <div className="flex items-center bg-blue-800">
                     <img
                       src="./Estimated.png"
                       width={80}
                       className="my-6 mx-8"
+                      alt="Estimated Conversion Rate"
                     ></img>
                     <p className="text-white w-50 text-lg font-semibold pr-20">
                       YOUR ESTIMATED CONVERSION RATE
@@ -184,7 +166,7 @@ export default function ProjectsPage() {
                         The anticipated percentage of website visitors who are
                         expected to take a desired action, such as making a
                         purchase or filling out a form. This action is based on
-                        the percentage that convert after entering site.
+                        the percentage that convert after entering the site.
                       </p>
                     </div>
                   </div>
@@ -195,9 +177,9 @@ export default function ProjectsPage() {
                     </p>
                     <Slider.Root
                       className="relative flex items-center w-full h-6 select-none"
-                      value={[conversionRate[0]]}
+                      value={[conversionRate]}
                       onValueChange={(newValues) =>
-                        setConversionRate([newValues[0]])
+                        setConversionRate(newValues[0])
                       }
                     >
                       <Slider.Track className="relative flex-grow h-2 bg-gray-200 rounded-full outline-none">
@@ -219,12 +201,13 @@ export default function ProjectsPage() {
 
             <div>
               <Card>
-                <div className=" space-y-4">
+                <div className="space-y-4">
                   <div className="flex items-center bg-blue-800">
                     <img
                       src="./Conversion.png"
                       width={70}
                       className="my-6 mx-8"
+                      alt="Conversion to Purchase Rate"
                     ></img>
                     <p className="text-white w-50 text-lg font-semibold">
                       YOUR CONVERSION TO PURCHASE RATE
@@ -234,7 +217,7 @@ export default function ProjectsPage() {
                         The expected percentage of conversions or desired
                         actions (e.g., form submissions) that are likely to
                         result in a completed purchase. This is the action of
-                        purchase after converting on website.
+                        purchase after converting on the website.
                       </p>
                     </div>
                   </div>
@@ -245,9 +228,9 @@ export default function ProjectsPage() {
                     </p>
                     <Slider.Root
                       className="relative flex items-center w-full h-6 select-none"
-                      value={[conversionToPurchase[0]]}
+                      value={[conversionToPurchase]}
                       onValueChange={(newValues) =>
-                        setConversionToPurchase([newValues[0]])
+                        setConversionToPurchase(newValues[0])
                       }
                     >
                       <Slider.Track className="relative flex-grow h-2 bg-gray-200 rounded-full outline-none">
@@ -269,12 +252,13 @@ export default function ProjectsPage() {
 
             <div>
               <Card>
-                <div className=" space-y-4">
+                <div className="space-y-4">
                   <div className="flex items-center bg-blue-800">
                     <img
                       src="./average.png"
                       width={70}
                       className="my-6 mx-8"
+                      alt="Average Sale Per Customer"
                     ></img>
                     <p className="text-white w-50 text-lg font-semibold">
                       AVERAGE SALE PER CUSTOMER
@@ -295,9 +279,9 @@ export default function ProjectsPage() {
                       className="relative flex items-center w-full h-6 select-none"
                       min={0}
                       max={1000}
-                      value={[averageSale[0]]}
+                      value={[averageSale]}
                       onValueChange={(newValues) =>
-                        setAverageSale([newValues[0]])
+                        setAverageSale(newValues[0])
                       }
                     >
                       <Slider.Track className="relative flex-grow h-2 bg-gray-200 rounded-full outline-none">
@@ -355,7 +339,7 @@ export default function ProjectsPage() {
                     <div className="flex text-left">
                       <div className="flex">
                         <div className="block text-gray-500 px-6 pb-2">
-                          <p>CONVENSION</p>
+                          <p>CONVERSION</p>
                           <p className="font-bold text-2xl text-black">
                             {conversions.toLocaleString("en-US", {
                               maximumFractionDigits: 0,
@@ -388,7 +372,7 @@ export default function ProjectsPage() {
                           <p>EXPECTED PROFIT</p>
                           <p className="font-bold text-2xl text-black">
                             ${" "}
-                            {(revenue - budget[0]).toLocaleString("en-US", {
+                            {(revenue - budget).toLocaleString("en-US", {
                               maximumFractionDigits: 0,
                             })}
                           </p>
@@ -399,7 +383,7 @@ export default function ProjectsPage() {
                 </div>
                 <div className="col-span-1 flex flex-col text-left">
                   <div className="p-5 flex-grow bg-red-500 py-24 px-16">
-                    <p className=" text-white">RETURN ON AD SPEND</p>
+                    <p className="text-white">RETURN ON AD SPEND</p>
                     <p className="font-semibold text-7xl text-white pb-10">
                       {roas.toFixed(0)} %
                     </p>
